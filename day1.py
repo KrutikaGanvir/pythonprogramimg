d5f52121d6d8a891f23c082c5f6051a9a53fa8a2
Krutika Ganvir <krutikaganvir@gmail.com>
2026-07-22 23:32:07 +0530

    intro

---
+++ day1.py
@@ -0,0 +1,66 @@
+
+# -----lets learn paython programming----
+
+
+# ---PYTHON HISTORY---
+"""
+🔸---.INTRODUCTION---
+1.Definition :
+ PYTHON is a high-level,interpreted and general-purpose programming language  that is easy to learn and use.
+
+ # --simple expalination--
+ python progrmaming  us to write   program in a simpleand readable way.
+
+ 2.Developer:
+ Developed by GUIDO VAN ROSSUM  and first released in 1991.
+
+ 3.Origin of the name:
+ Python was named after the British comedy television show "Monty Python's Flying Circus."
+
+
+ # -- Features--
+1.Easy to learn and read
+2.High level languafge
+3.Interpreated language
+4.Platform independent
+5.Dyanamically Typed
+   🔹you do  not  need to  declare variable  type explicitly.
+   🔹 Example:
+              x = 10
+              x = "hello"
+
+#  ---Uses---
+1.Web Development
+2.Artificial Inteliigence
+3.Machine learning
+4.Automation
+
+------------------------*********--------------------------
+
+
+2 ----First python program-----
+
+print("Hello World")
+
+✅Explaination
+🔹 Print() -> Display Output on the screen
+🔹 "hello World -> A string (text)
+
+3.----COMMENTS IN PYTHON----
+ 🔹omments are  used to explain code.they are not excecuted.
+
+ 🔹For comment we use # hashtag (hash symbol) for single line code .
+
+ 🔹In Python, multi-line comments are commonly written using triple quotes (''')
+
+ 🔹 Example:
+
+ #this is a comment
+ print("Hello World")
+
+"""
+
+
+
+
+
