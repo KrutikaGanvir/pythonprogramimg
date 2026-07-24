7fd4361e7f075ea03b0735870fa9f8ceda7f21a7
Krutika Ganvir <krutikaganvir@gmail.com>
2026-07-24 20:30:40 +0530

    variables

---
+++ day2.py
@@ -0,0 +1,27 @@
+"""
+🔸---Variables in python----
+
+ A Variables is a named memory location used to stored  data
+ 
+ in simples words:
+  A variables is a containerusedx tostore  data in a program.
+
+  🔹For Example:
+
+
+name = "Krutika"
+age = 21
+marks = 85.5
+print(name)
+print(age)
+print(marks)
+
+🔹Explaination
+'name' stores a string value.
+'age' stores an integer value.
+`marks` stores a decimal value.
+`print()` display the values on the screen.
+
+
+"""
+
