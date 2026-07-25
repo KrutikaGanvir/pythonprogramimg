e2d5808221619199bb4cc132e3298579fe6937ba
Krutika Ganvir <krutikaganvir@gmail.com>
2026-07-25 22:38:31 +0530

    dyanamiv typing in python

--- day2.py
+++ day2.py
@@ -22,6 +22,48 @@ print(marks)
 `marks` stores a decimal value.
 `print()` display the values on the screen.
 
+2---How Variables Work in Memory---
+ When a variable is created , python stores the values in the computer's memory and assigns a 
+ name to that memory location.
+  🔹For Example:
+
+    x = 10
+   🔹10 is stored in memory.
+
+   🔹 x refers to the memory location containing 10.
+
+   Variable      Value in Memory
+   x   ───────────►   10
+
+3.. Dynamic Typing in Python---
+
+Dynamic typing means you do not need to declare the data type of a variable python automatically determines the type based on the value assigned.
+
+🔹 Example no 1:
+  x = 10
+  x = 3.14
+  x ="python"
+  print(x)
+
+  output:
+  python
+
+🔹 Example no 2:
+    x = 10 print(type(x)) 
+    x = "Python" print(type(x))
+
+    output:
+    <class 'int'>
+    <class 'str'>
+
+
+
+  key points:
+  🔹No need to specify the data type.
+  🔹A variable can store diffrent types of values.
+  🔹Python automatically detects the variables data type.
+
+
 
 """
 
