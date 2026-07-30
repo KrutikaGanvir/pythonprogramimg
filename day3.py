9d1e4cd5ddcc207ac658e059d54000609fc0a02b
Krutika Ganvir <krutikaganvir@gmail.com>
2026-07-30 21:10:29 +0530

    datatype

---
+++ day3.py
@@ -0,0 +1,63 @@
+'''
+-----DATATYPE------
+
+10.Data type in python----
+
+A data type specifies the type of value that a variable can store in Python.
+
+
+Data Type	    Example
+int	              10
+float	         3.14
+str	           "Hello"
+bool	       True, False
+list	       [1, 2, 3]
+tuple	       (1, 2, 3)
+set	           {1, 2, 3}
+dict	{"name": "Dipanshu", "age": 20}
+
+#🔹 Example:
+
+x     =   10     #int
+y     =  3.14    #float
+name  = "Python" #str
+is_pass = True   #bool
+
+11. Primitive and Non-Primitive Data Types
+----Primitive Data Types----
+
+◽Primitive data type store a single value.
+
+#🔹 Example:
+  🔸int  
+  🔸float  
+  🔸str  
+  🔸bool  
+
+  
+  age = 10
+  price = 99.9
+  name = "Krutika"
+  is_pass = True
+
+  ----Non-Primitive Data Types----
+
+  ◽Non-primitive data type store multiple values or collection of data.
+
+  #🔹 Example:
+   🔸 list
+   🔸 tuple
+   🔸 set
+   🔸 dict
+
+   number = [10,20,30]
+   student = {"name": "Krutika","age":20}
+
+12. Number Data Types
+
+◽ Number data type are used to store numeric values in Python.
+◽Python has three important number data types: int, float, and complex
+ Type of Number Data Types
+ 1.int - Stores Whole numbers.
+ 2.Float - Store decimal(Floating-point)numbers
+'''
