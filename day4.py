8e47fb9a850dbd07b280b086fdb85ba883165a19
Krutika Ganvir <krutikaganvir@gmail.com>
2026-08-02 22:24:44 +0530

    typefunction

---
+++ day4.py
@@ -0,0 +1,24 @@
+"""
+17. type() Function
+◽ The type() function is used to find type of a variables or value in python.
+
+Syntax:
+type (object)
+
+# 🔹Examples:
+
+x = 10 
+y = 3.14
+name = "Python"
+print(type(x)) 
+print(type(y))
+print(type(name))
+
+Output:
+<class 'int'>
+<class 'float'>
+<class 'str'>
+
+
+
+"""
