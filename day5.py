8d4adee32776f3f49cd5a269a291754da17c3a9d
Krutika Ganvir <krutikaganvir@gmail.com>
2026-08-08 23:17:54 +0530

    operators

---
+++ day5.py
@@ -0,0 +1,27 @@
+"""
+    22.OPERATION IN PYTHION-------------
+
+◽Operation are special sysmbol used to perform operations on values and variables in Python
+
+🔹Example:
+a = 10
+b = 5
+print(a + b)
+
+🔸Types of Operators
+
+1.Arithmatic --> +, -,*, /, %
+2.Comparison --> ==, !==, >,<
+3.Logical --> and, or, not
+4.Assignment --> = , +=, -=
+5.Membership --> in ,not in
+6.Identity --> is, is not
+
+🔹Example:
+
+a = 10
+b = 5
+
+print( a + b)
+print( a > b)
+    """
