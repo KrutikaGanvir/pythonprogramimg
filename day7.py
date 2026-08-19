c9d645b2e9b5d750f94d72125412e9365a0df8a6
Krutika Ganvir <krutikaganvir@gmail.com>
2026-08-19 22:02:19 +0530

    condtional statement

---
+++ day7.py
@@ -0,0 +1,22 @@
+#--------------Conditional Statements in Python-------------------
+
+ #🔸1. What is a Conditional Statement?
+'''
+Conditional statement are used to make decision in a Python propgram based on wheather a condition is True or False.
+
+🔹Simple Example:
+
+age = 18
+
+if age >= 18:
+print(""you are eligible to vote")
+
+Types of Conditional Statements
+
+1.if Statement - Execute ciode when a condition is  true.
+2.if-else Statement - Chooses between two conditions.
+3.if-else-else Statement - Check multiple condition.
+4.Nested if Statement - An if statement inside another if.
+5.Conditional Expression (Ternary Operator)-Short form  of if - else .
+
+'''
