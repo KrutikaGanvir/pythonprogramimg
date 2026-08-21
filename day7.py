1aa0f2f3c394e4a00e94e1b135d20eaba780d6b0
Krutika Ganvir <krutikaganvir@gmail.com>
2026-08-21 21:27:53 +0530

    use

--- day7.py
+++ day7.py
@@ -19,4 +19,17 @@ Types of Conditional Statements
 4.Nested if Statement - An if statement inside another if.
 5.Conditional Expression (Ternary Operator)-Short form  of if - else .
 
+
+2. Why Do We Use Conditional Statements?
+
+Conditional statement are used to make decision in aprogram based on a given condition.
+
+They help the program excecute code for diffrent situations.
+
+age = 20
+if age >= 18:
+    print("Eligible to vote")
+
+else:
+    print("Not eligible to vote")    
 '''
