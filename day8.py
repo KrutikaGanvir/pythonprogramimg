84953998ed3096626c80c115a7641eff24ecad32
Krutika Ganvir <krutikaganvir@gmail.com>
2026-09-02 23:11:56 +0530

    problems_02 done

--- day8.py
+++ day8.py
@@ -44,7 +44,7 @@ else:
 
 
 🔸Program 4: Discount Calculator  
-'''
+
 
 amount = 6000
 
@@ -56,3 +56,49 @@ else:
 final_amount = amount - discount
 print("Discount : ", discount)
 print("Final amount to be paid: ", final_amount)
+
+🔸Program 5: Check leap Year
+
+
+year = 2024
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year")
+else:
+    print(year, "is not a leap year")
+
+ 🔸Program 6: Traffic Signal   
+
+signal = "red"
+
+if signal == "red":
+    print("Stop")
+elif signal == "yellow":
+    print("Ready")
+elif signal == "green":
+    print("Go")
+else:
+    print("Invalid signal")
+
+
+
+🔸Program 7: Number Dvisible By 5 and 11
+   
+
+num = 55
+if num % 5 == 0 and num % 11 == 0:
+    print(num, "is divisible by both 5 and 11")
+else:
+    print(num, "is not divisible by both 5 and 11")
+
+
+🔸Program 8: Basic Login System
+
+username = "krutika"
+password = "krut@123"
+
+if username == "krutika" and password == "krut@123":
+    print("Login successful!")
+else:
+    print("Invalid credentials!")
+'''
