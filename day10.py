d29114bdea543eaec2178adba0dcc8a19382ecc9
Krutika Ganvir <krutikaganvir@gmail.com>
2026-09-08 20:11:56 +0530

    todayswork done

---
+++ day10.py
@@ -0,0 +1,19 @@
+'''
+#  -------------------------------Loops in Python---------------------------------------
+
+1.Concept of Eplanation 
+
+🔸Meaning of looop  
+A loop is used to repeat a block of code agian and again Without loops, we need to write the same many times.
+
+
+EX:If we want to print  numbers from 1 to 100, writing 100 printnsatement is not a good method Aloop can do this work easily
+
+
+
+🔹Simple Example:
+
+
+'''
+for i in range(1,100):
+    print(i)
