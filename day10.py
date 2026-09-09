6ec38d1b849dd3372c97d3984d4ee511f7030fb6
Krutika Ganvir <krutikaganvir@gmail.com>
2026-09-09 22:58:56 +0530

    todays works

--- day10.py
+++ day10.py
@@ -14,6 +14,34 @@ EX:If we want to print  numbers from 1 to 100, writing 100 printnsatement is not
 🔹Simple Example:
 
 
-'''
 for i in range(1,100):
+    print(i)
+
+
+🔸 Why Loops Are Used in Python
+
+Loops are used  to repeat  a block of code multiople times without writing the same code again and again.
+
+Main Uses of Loops:
+
+1. To repeat a task mutliple times.
+2. To process multiple value one by one.
+3. To save time and reduce code repetition
+4. To perform calcution repeatedly.
+5. To work with lists , strings, and other collections.
+
+
+Example:
+ Without  loop:
+
+ print(1)
+ print(2)
+ print(3)
+ print(4)
+ print(5)
+
+ Using loop:
+'''
+
+for i in range(1, 6):
     print(i)
