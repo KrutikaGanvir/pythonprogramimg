51b33da9f7d5e4a023d2d953f0e55f7ef589139a
Krutika Ganvir <krutikaganvir@gmail.com>
2026-09-19 23:24:21 +0530

    Meaning of string

---
+++ day13.py
@@ -0,0 +1,33 @@
+'''
+--------------- String in python ------------------------\
+
+
+1. Concept Explanation
+
+🔸Meaning of String
+
+A string is a sequence of character (letters , numbers, symbols, or spaces)
+enclosed inside quotes.
+
+▫️Example:
+
+  name = "Krutika"
+  city = "Nagpur"
+
+  Here,"Krutika"  and "Nagpur" are strings.
+
+▪️Types of Quotes
+
+a = 'hello'
+b = "hello"
+c = """hello"""
+
+🔺Important Point:
+
+String in Python are immutable, which means their characters cannot be changed directly  after creation
+
+
+
+
+
+'''
