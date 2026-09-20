ec4cc87433435bcf8415382a5f44b4e7fd84c11b
Krutika Ganvir <krutikaganvir@gmail.com>
2026-09-20 21:12:30 +0530

    string slicing

--- day13.py
+++ day13.py
@@ -18,7 +18,7 @@ enclosed inside quotes.
 
 ▪️Types of Quotes
 
-a = 'hello'
+a = 'hello':
 b = "hello"
 c = """hello"""
 
@@ -27,7 +27,69 @@ c = """hello"""
 String in Python are immutable, which means their characters cannot be changed directly  after creation
 
 
+3.String Indexing
+
+String indexing means accessing individual characters of a string using their position (index).
+
+Python indexing start from 0.
 
 
+▫️Example:
+
+name = "Python"
+print(name[0])
+print(name[1])
+print(name[5])
+
+Index Position:
+
+String: P  y  t  h  o   n 
+
+Index:  0  1  2  3  4   5
+
+Python  also supports negative indexing:
+
+String:     p   y   t   h   o   n
+Negative:  -6   -5  -4  -3  -2   -1
+
+▫️Example
+
+
+name = "Python"
+print(name[-1])
+print(name[-2])
+
+
+Easy Trick: Positive index starts from 0 (left), negative index start from -1 (right)
+
+
+4.String Slicing
+
+String slicing means extracting a part of a string using index position.
+
+Syntax:
+
+string[ start : stop: step]
+
+▫️Example:
+
+name  = "Python"
+print(name[0:3])
+print(name[2:5])
+
+
+▫️Example no:2
+
+name = "Python"
+
+print(name[:3])
+print(name[2:])
+print(name[::2])
+print(name[:: -1])
+
+
+🔹start =  where to start
+🔹stop  = where to stop(not  include)
+🔹step = how many position to jump
 
 '''
